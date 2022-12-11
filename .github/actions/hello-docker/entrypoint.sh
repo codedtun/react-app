9c2464bb2db2a6bd9fd93f1e081a4d5df39a6ee0
codedtun <tunde_olomu@programmer.net>
2022-12-11 09:02:04 +0000

    new docker file added

---
+++ .github/actions/hello-docker/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -l
+
+echo "::debug ::Debug Message"
+echo "::warning ::Warning Message"
+echo "::error ::Error Message"
+
+echo "::add-mask::$1"
+
+echo "Hello $1"
+time=$(date)
+echo "::set-output name=time::$time"
+
+echo "::group::Some expandable logs"
+echo 'some output'
+echo 'some stuff'
+echo 'something'
+echo ''::endgroup::'
+
+echo '::set-env name=HELLO::hello'
