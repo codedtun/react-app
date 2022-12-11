1864049604eb9f7c3418c38c7638a25b72197a4b
codedtun <tunde_olomu@programmer.net>
2022-12-11 07:24:18 +0000

    feat: added new yml file

---
+++ .github/actions/hello/index.js
@@ -0,0 +1,16 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+try {
+  // throw new Error("some error has prevented the code from running");
+
+  const name = core.getInput("who-to-greet");
+  console.log("hello ${name}");
+
+  const time = new Date();
+  core.setOutput("time", time.toTimeString());
+
+  console.log(JSON.stringify(github, null, "\t"));
+} catch (error) {
+  core.setFailed(error.message);
+}
