66e08d71fc60dbbd449307949c11ba5039997467
codedtun <tunde_olomu@programmer.net>
2022-12-11 07:41:19 +0000

    feat: added new yml file

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -1877,15 +1877,24 @@ const core = __webpack_require__(470);
 const github = __webpack_require__(469);
 
 try {
-  throw new Error("some error has prevented the code from running");
+  // throw new Error("some error has prevented the code from running");
+
+  core.debug("Debug Message");
+  core.warning("Warning Message");
+  core.error("Error Message");
 
   const name = core.getInput("who-to-greet");
+  core.setSecret(name);
   console.log("hello ${name}");
 
   const time = new Date();
   core.setOutput("time", time.toTimeString());
 
+  core.startGroup("Logging github object");
   console.log(JSON.stringify(github, null, "\t"));
+  core.endGroup();
+
+  core.exportVariable("Hello", "hello");
 } catch (error) {
   core.setFailed(error.message);
 }
--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -2,15 +2,24 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
-  throw new Error("some error has prevented the code from running");
+  // throw new Error("some error has prevented the code from running");
+
+  core.debug("Debug Message");
+  core.warning("Warning Message");
+  core.error("Error Message");
 
   const name = core.getInput("who-to-greet");
+  core.setSecret(name);
   console.log("hello ${name}");
 
   const time = new Date();
   core.setOutput("time", time.toTimeString());
 
+  core.startGroup("Logging github object");
   console.log(JSON.stringify(github, null, "\t"));
+  core.endGroup();
+
+  core.exportVariable("Hello", "hello");
 } catch (error) {
   core.setFailed(error.message);
 }
